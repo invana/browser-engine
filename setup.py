a2fa932e99a742a3ff7b5dabb92afa2a3d752945
Ravi Raja Merugu <rrmerugu@gmail.com>
2019-10-10 16:11:42 +0530

    static variable added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='browser-engine',
-    version='0.0.11',
+    version='0.0.12',
     description='Web Automation and User behaviour simulations made easy with YAML configurations.',
     author='Ravi Raja Merugu',
     author_email='ravi@invanalabs.ai',
